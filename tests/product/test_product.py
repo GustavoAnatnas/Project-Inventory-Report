edb355ac7d1727d85336c6185693b341c10aa50c
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-02-01 20:17:43 -0300

    Cria teste que testa o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "1",
+        "name",
+        "brand",
+        "01-02-2023",
+        "01-02-2024",
+        "1",
+        "storage"
+    )
+
+    assert product.id == "1"
+    assert product.nome_do_produto == "name"
+    assert product.nome_da_empresa == "brand"
+    assert product.data_de_fabricacao == "01-02-2023"
+    assert product.data_de_validade == "01-02-2024"
+    assert product.numero_de_serie == "1"
+    assert product.instrucoes_de_armazenamento == "storage"
