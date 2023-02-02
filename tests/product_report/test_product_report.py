2f7e6d423c530124657da10ceef31ad147deb9d7
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-02-02 14:16:57 -0300

    Cria teste para o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "1",
+        "Farinha",
+        "Farinini",
+        "01-02-2023",
+        "02-02-2024",
+        "12345",
+        "em local fresco e seco",
+    )
+
+    assert (product.__repr__()) == (
+        "O produto Farinha"
+        " fabricado em 01-02-2023"
+        " por Farinini com validade"
+        " até 02-02-2024"
+        " precisa ser armazenado em local fresco e seco."
+    )
