ace1796ecee86991074022d7c187d513796ded05
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-02-01 22:42:35 -0300

    Cria classe responsavel por gerar a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
+from inventory_report.reports.simple_report import SimpleReport
+# from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @classmethod
+    def generate(cls, inventory_list):
+        simple_report = SimpleReport.generate(inventory_list)
+        company_counts = dict()
+        for item in inventory_list:
+            company_name = item["nome_da_empresa"]
+            if company_name in company_counts:
+                company_counts[company_name] += 1
+            else:
+                company_counts[company_name] = 1
+        company_stock = ""
+        for company, count in company_counts.items():
+            company_stock += f"- {company}: {count}\n"
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{company_stock}\n"
+        )
