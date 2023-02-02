34e8ab99b8ce7eb93408f381876b0f06d80e6bd0
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-02-02 13:28:55 -0300

    Modifica classe CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,11 +1,11 @@
 from inventory_report.reports.simple_report import SimpleReport
-# from collections import Counter
 
 
 class CompleteReport:
     @classmethod
     def generate(cls, inventory_list):
         simple_report = SimpleReport.generate(inventory_list)
+
         company_counts = dict()
         for item in inventory_list:
             company_name = item["nome_da_empresa"]
@@ -13,11 +13,13 @@ class CompleteReport:
                 company_counts[company_name] += 1
             else:
                 company_counts[company_name] = 1
+
         company_stock = ""
         for company, count in company_counts.items():
             company_stock += f"- {company}: {count}\n"
+
         return (
             f"{simple_report}\n"
             f"Produtos estocados por empresa:\n"
-            f"{company_stock}\n"
+            f"{company_stock}"
         )
